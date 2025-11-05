8e40d334e01fa070c6eb60edeb6ec231a77db8fe
ddmdros <diogome_deiros@hotmail.com>
2025-11-05 09:18:58 -0300

    tp2.1 html

---
+++ HTML_CSS_I/Aula/11/03/script.js
@@ -0,0 +1,9 @@
+let ano;
+
+if ((ano % 4 === 0) && (ano % 100 === 0) && (ano % 400 === 0)){
+    alert("O ano " + ano + " é bissexto");
+} else if (ano % 4 === 0){
+    alert("O ano " + ano + " é bissexto");
+} else {
+    alert ("O ano " + ano + " não é bissexto");
+}
